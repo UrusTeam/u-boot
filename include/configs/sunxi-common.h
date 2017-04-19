5a6114c146cc6d4850d7303d4f9dad3050194cce
hiro2233 <htakey@gmail.com>
2017-04-19 08:11:07 -0400

    configs: sunxi common, enabled urus logo.

--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -286,6 +286,7 @@ extern int soft_i2c_gpio_scl;
 #define CONFIG_VIDEO_SUNXI
 
 #define CONFIG_VIDEO_LOGO
+#define CONFIG_VIDEO_BMP_LOGO
 #define CONFIG_VIDEO_STD_TIMINGS
 #define CONFIG_I2C_EDID
 #define VIDEO_LINE_LEN (pGD->plnSizeX)
